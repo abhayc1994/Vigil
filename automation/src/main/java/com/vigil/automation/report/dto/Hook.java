77cd920cee1ac7ac2f8628d181ac3e98f101523c
Abhay Chaudhary <abhayc1994@gmail.com>
2022-09-28 02:19:57 +0530

    report builder base

---
+++ automation/src/main/java/com/vigil/automation/report/dto/Hook.java
@@ -0,0 +1,31 @@
+package com.vigil.automation.report.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.time.Duration;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Hook {
+    private Result result = new Result();
+    private List<Embedding> embeddings;
+
+    public Duration getDuration() {
+        return Duration.ofNanos(result.getDuration());
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public List<Embedding> getEmbeddings() {
+        return embeddings;
+    }
+
+    public void setEmbeddings(List<Embedding> embeddings) {
+        this.embeddings = embeddings;
+    }
+}
---
+++ automation/src/main/java/com/vigil/automation/report/dto/Row.java
@@ -0,0 +1,31 @@
+package com.vigil.automation.report.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Row {
+    private String id;
+    private List<String> cells;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getCells() {
+        return cells;
+    }
+
+    public void setCells(List<String> cells) {
+        this.cells = cells;
+    }
+
+    @Override
+    public String toString() {
+        return getCells().toString();
+    }
+}
