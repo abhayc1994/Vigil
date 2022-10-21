d71a1b2eded080defb3556fefaf09024ba2b57a1
Abhay Chaudhary <abhayc1994@gmail.com>
2022-10-22 02:30:19 +0530

    cors

--- automation/src/main/java/com/vigil/automation/security/ApplicationSecurityConfig.java
+++ automation/src/main/java/com/vigil/automation/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vigil.automation.security;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +29,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		  .httpBasic()
 		  .and()
 		  .csrf().disable();
+   }
+
+   @Bean
+   CorsConfigurationSource corsConfigurationSource() {
+	  CorsConfiguration configuration = new CorsConfiguration();
+	  configuration.setAllowedOrigins(Arrays.asList("https://vigil-be.herokuapp.com/"));
+	  configuration.setAllowedMethods(
+		  Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+	  configuration.setAllowedHeaders(
+		  Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+			  "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+			  "Cache-Control", "Content-Type"));
+	  final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	  source.registerCorsConfiguration("/**", configuration);
+	  return source;
    }
 
    @Bean
